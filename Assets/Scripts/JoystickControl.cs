4331fb6a59abb7df65daee3130b9238cea900272
Rodrigo Santos Alvares <ralvares@EGSBR.local>
2020-03-23 03:42:32 -0300

    Sword shield skill

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JoystickControl : MonoBehaviour{
+
+
     public float speed;
     public FixedJoystick fixedJoystick;
     public ButtonAction basicAttack;
@@ -10,6 +12,14 @@ public class JoystickControl : MonoBehaviour{
     public ButtonAction skillOne;
     public ButtonAction skillTwo;
     public Rigidbody rb;
+
+    public bool isAxe;
+    public bool isSword;
+    public bool isDagger;
+
+    public GameObject swordSkill;
+    private GameObject weaponSkill;
+    private bool skillUsed = false;
 
     private void Start(){
     }
@@ -24,7 +34,20 @@ public class JoystickControl : MonoBehaviour{
         }
         //SpecialAttack
         if (specialAttack.action()){
-            Debug.Log("SpecialAttack");
+            if (isAxe){
+                Debug.Log("Life Steal");
+            }else if (isSword){
+                //Procura por weaponSkill na cena para permitir a ação
+                if (GameObject.FindGameObjectsWithTag("WeaponSkill").Length == 0){
+                    skillUsed = true;
+                    weaponSkill = Instantiate(swordSkill, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+                    weaponSkill.transform.parent = GameObject.Find("Player").transform;
+                    Destroy(weaponSkill, 5);
+                }
+                Debug.Log("Shield");
+            }else if (isDagger){
+                Debug.Log("Dash");
+            }
         }
 
         //SkillOne
