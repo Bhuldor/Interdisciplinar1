70874482e14b1a15a846c3f6fbfb97a85f5e2854
Rodrigo Alvares <rsa.rodrigoalvares@gmail.com>
2020-04-25 14:46:21 -0300

    Weapons Skills -except the main attack to be done with player animations

---
+++ Assets/Scripts/Abstracts/CharacterStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterStatus : Status
+{
+    protected float hitPoints;
+    public CharacterStatus(float atk, float def, float spd, float hp) : base(atk, def, spd)
+    {
+        attack = atk;
+        defense = def;
+        speed = spd;
+        hitPoints = hp;
+    }
+
+    public float getHp() { return hitPoints; }
+
+    public void setHp(float hp) { this.hitPoints = hp; }
+}
---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : CharacterStatus
+{
+    private float actualExp;
+    private float nextLevelExp;
+    private int level;
+
+    public PlayerStatus(float atk, float def, float spd, float hp, float actualExp, float nextLevelExp, int lvl) : base(atk, def, spd, hp)
+    {
+        attack = atk;
+        defense = def;
+        speed = spd;
+        hitPoints = hp;
+        this.actualExp = actualExp;
+        this.nextLevelExp = nextLevelExp;
+        this.level = lvl;
+    }
+
+    public void levelUp(){
+        level++;
+        hitPoints += 5;
+    }
+
+    public void levelUpAttribute(int whichAtt){
+        //whichAtt representa a escolha do atributo ao player passar de nível
+        //0 - Attack
+        //1 - Defense
+        //2 - Speed
+        switch (whichAtt){
+            case 0:
+                attack++;
+                break;
+            case 1:
+                defense++;
+                break;
+            case 2:
+                speed++;
+                break;
+        }
+    }
+}
