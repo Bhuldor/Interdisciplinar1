6727da41b1bc1bdfb508eb69ff47198ff289c107
Rodrigo Alvares <rsa.rodrigoalvares@gmail.com>
2020-04-30 17:49:33 -0300

    Temp Player

---
+++ Assets/Scripts/DamageController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    [Header("Settings")]
+    public int damageToGive;
+    public string whoToHurt = "Player";
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon" && transform.tag == "Enemy"){
+            transform.GetComponent<EnemyHealthManager>().HurtEnemy(other.gameObject.GetComponent<DamageController>().damageToGive);
+        }
+
+        if (other.gameObject.tag == whoToHurt)
+        {
+            switch (whoToHurt)
+            {
+                case "Player":
+                    other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(damageToGive);
+                    break;
+                case "Enemy":
+                    other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+                    break;
+            }
+            
+        }
+    }
+}
