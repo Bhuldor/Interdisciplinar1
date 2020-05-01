01375b9463856fe8f1323730cb95b406af7eafe0
Rodrigo Alvares <rsa.rodrigoalvares@gmail.com>
2020-04-30 21:11:34 -0300

    Temp Player

--- Assets/Scripts/DamageController.cs
+++ Assets/Scripts/DamageController.cs
@@ -8,6 +8,7 @@ public class DamageController : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "Weapon" && transform.tag == "Enemy"){
             transform.GetComponent<EnemyHealthManager>().HurtEnemy(other.gameObject.GetComponent<DamageController>().damageToGive);
         }
